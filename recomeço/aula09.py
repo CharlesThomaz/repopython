22e882e4bf6a067457a1ed8e1844b9d1e2e7b512
CharlesThomaz <charlesthomaz87@gmail.com>
2023-10-14 02:35:54 -0300

    Create aula09.py

---
+++ recomeço/aula09.py
@@ -0,0 +1,8 @@
+
+frase = '''O operador in é usado em Python para verificar
+se um valor ou elemento está presente em uma sequência, 
+como uma lista, tupla, string, dicionário ou conjunto. 
+Ele retorna True se o elemento estiver na sequência e 
+False se não estiver. A sintaxe geral do operador in é a seguinte.'''
+dividido = ( frase.split())
+print(dividido[5][1])
